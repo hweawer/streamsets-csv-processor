675e557a7390fbc7d0aa7c77978222cda4fcbd73
Aliaksandr Husakouski <Aliaksandr_Husakouski@epam.com>
2020-01-25 00:32:21 +0300

    Maven Wrapper, fix deployment script

--- create-custom-streamsets-processor.sh
+++ create-custom-streamsets-processor.sh
@@ -1,6 +1,6 @@
 ./mvnw clean install
 
+docker exec sdc bash -c 'cd /opt/streamsets-datacollector-user-libs && mkdir streamsets-csv-processor && cd streamsets-csv-processor && mkdir lib'
+
 docker cp ./target/streamsets-csv-processor-1.0-SNAPSHOT.jar sdc:/opt/streamsets-datacollector-user-libs/streamsets-csv-processor/lib
-docker cp ./target/streamsets-csv-processor-1.0-SNAPSHOT.jar sdc:/opt/streamsets-datacollector-3.13.0/user-libs/streamsets-csv-processor/lib
-docker cp ./lib/geohash-1.4.0.jar sdc:/opt/streamsets-datacollector-3.13.0/user-libs/streamsets-csv-processor/lib
-docker cp ./lib/geohash-1.4.0.jar sdc:/opt/streamsets-datacollector-3.13.0/user-libs/streamsets-csv-processor/lib
+docker cp ./lib/geohash-1.4.0.jar sdc:/opt/streamsets-datacollector-user-libs/streamsets-csv-processor/lib
